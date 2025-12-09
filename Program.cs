0280bdeb6d905ef9200bd7ee6aa47d5dae810e33
AbdumubinAmakov <abdymybinamakov@gmail.com>
2025-12-09 17:42:33 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Task1
+void PrintFromNumberToOne(int number){
+    for (int i = number; i >= 1; i--)
+    {
+        Console.WriteLine(i);
+    }
+}
+int a=int.Parse(Console.ReadLine());
+PrintFromNumberToOne(a);*/
+
+
+//Task2
+/*void Sum(int a, int b, int c, int d){
+    int Sum=a+b+c+d;
+    Console.WriteLine(Sum);
+}
+int a=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+int c=int.Parse(Console.ReadLine());
+int d=int.Parse(Console.ReadLine());
+Sum(a,b,c,d);*/
+
+//Task3
+/*void Reverse(int n)
+{
+    for (int i=n; i>0; i /= 10)
+    {
+       Console.Write(i%10) ;
+    }
+}
+int n=int.Parse(Console.ReadLine());
+Reverse(n);*/
+
+//Task4
+//Section1
+void PrintStars(int n){
+    for(int i=n; i>0; i--){
+    Console.Write('*');
+    }
+    Console.WriteLine();
+}
+// int a=int.Parse(Console.ReadLine()!);
+//PrintStars(a);
+//Section2
+// void PrintSquare(int n)
+// {
+//     for (int i = 0; i <n; i++)
+//     {
+//         PrintStars(n);
+        
+//         Console.WriteLine();
+//     }
+// }
+// int a=int.Parse(Console.ReadLine()!);
+// PrintSquare(a);
+
+//Section3
+void PrintRectangle(int width, int height)
+{
+    for (int i = 1; i <= height; i++)
+    {
+         PrintStars(width);
+    }
+}
+int width=int.Parse(Console.ReadLine());
+int height=int.Parse(Console.ReadLine());
+PrintRectangle(width, height);
+
